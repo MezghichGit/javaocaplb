f372046f007724d40854edf17240daefa27818c4
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-11-29 10:31:42 +0100

    chapter 1 - part 1 & 2

---
+++ src/plb_oca/chapter1/Animal.java
@@ -0,0 +1,17 @@
+package plb_oca.chapter1;
+
+public class Animal {
+
+	// attributs d'instance
+	String nom;
+	int age;
+	// attributs de classe
+	
+	static int maxAge=100;
+	
+	//méthode d'instance
+}
+/*
+ class Oiseau{
+	
+}*/
